c740507f53bc84e72dbfd5018fd968b98cdc93fe
Othniel Chan <othnielhr.chan@gmail.com>
2022-06-28 17:56:16 -0500

    changed express.static path

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('../client/dist/'));
+app.use(express.static('client/dist/'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
