85f6b3a6cb34782983706279794ad1be23cfc27d
Othniel Chan <othnielhr.chan@gmail.com>
2022-06-30 17:07:33 -0500

    formatted free form text

--- client/src/components/overview/ProductInformationFreetext.jsx
+++ client/src/components/overview/ProductInformationFreetext.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 var ProductInformationFreetext = (props) => (
   <div className="freeFormText">
+    <h4>Slogan</h4>
+    <p>
     Free form text that may or may not appear depending on the product information provided. should be positioned below the image gallery...
+    </p>
   </div>
 );
 
