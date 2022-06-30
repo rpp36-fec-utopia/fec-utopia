71bdeb301c1fd2cd17a8062e2aa9bf7e9a776c3c
Chris <chrisbaharians@gmail.com>
2022-06-29 21:29:15 -0600

    Committing to merge

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import QASection from './components/questionsAndAnswers/QASection.jsx'
 import RelatedProducts from './components/relatedItems/RelatedProducts.jsx'
-import Overview from './componenets/overview/Overview.jsx';
+import Overview from './components/overview/Overview.jsx';
 
 class App extends React.Component {
   constructor(props) {
