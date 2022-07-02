dea9bef6c1b7db8bd6751114b9fe7d5cf9e19dc1
Othniel Chan <othnielhr.chan@gmail.com>
2022-07-02 14:39:12 -0500

    mini commit

--- client/src/components/overview/ProductInformationFreetext.jsx
+++ client/src/components/overview/ProductInformationFreetext.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 var ProductInformationFreetext = (props) => (
   <div className="freeFormText">
-    <h4>Slogan</h4>
+    <h4>{props.slogan}</h4>
     <p>
-    Free form text that may or may not appear depending on the product information provided. should be positioned below the image gallery...
+    Free form text that may or may not appear depending on the product information provided. should be positioned below the image gallery...{props.desc}
     </p>
+    <p>{props.feats}</p>
   </div>
 );
 
