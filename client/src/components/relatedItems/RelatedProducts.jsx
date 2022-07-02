dad0d530980600ae2ec82dcc6358e71134e820cd
Chris <chrisbaharians@gmail.com>
2022-07-02 14:31:34 -0600

    committing to merge

--- client/src/components/relatedItems/RelatedProducts.jsx
+++ client/src/components/relatedItems/RelatedProducts.jsx
@@ -6,8 +6,13 @@ import axios from 'axios';
 class RelatedProducts extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      products:
+    }
   }
+
+  // plan is to make a request to the api and receive some dummy data back so I can plug it in and create some cards
+  // later whenever othniel is ready with his data then I can sort by whatever category and refactor into non dummy data
 
   /*
   This is where I plan on fetching the products from the API.
@@ -30,5 +35,6 @@ class RelatedProducts extends React.Component {
     )
   }
 }
+
 
 export default RelatedProducts;
