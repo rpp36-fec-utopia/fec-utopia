003326ef6b8f883d77904e6ec51654237398f5cd
AaronW <aaronwang96@hotmail.com>
2022-07-03 20:52:06 -0700

    Answers List Item setup

--- client/src/components/questionsAndAnswers/AnswersListItem.jsx
+++ client/src/components/questionsAndAnswers/AnswersListItem.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 class AnswersListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: ""
+      date: "",
+      helpful: 0
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.incrementHelpful = this.incrementHelpful.bind(this);
   }
 
   componentDidMount() {
     const date = new Date(this.props.answers[this.props.ans].date);
     this.setState({
-      date: date.toLocaleDateString()
+      date: date.toLocaleDateString(),
+      helpful: this.props.answers[this.props.ans].helpfulness
+    })
+  }
+
+  handleClick() {
+    axios.post('/qa/answers/helpful', {
+      answers_id: this.props.answers[this.props.ans].id,
+    })
+    .then(result => console.log('SUCCESS'))
+    .then(this.incrementHelpful)
+  }
+
+  incrementHelpful() {
+    this.setState({
+      helpful: this.state.helpful + 1
     })
   }
 
@@ -21,7 +40,7 @@ class AnswersListItem extends React.Component {
         <div>{this.props.i+1}: {this.props.answers[this.props.ans].body}</div>
         <div>
           by {this.props.answers[this.props.ans].answerer_name}, {this.state.date}
-          <button>Helpful</button>
+          <button onClick={this.handleClick}>Helpful ({this.state.helpful})</button>
           <button>Report</button>
         </div>
       </div>
