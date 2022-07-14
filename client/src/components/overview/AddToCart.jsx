3ff359fc39767a9233f96fed9d61c5ea7318c1b6
Othniel Chan <othnielhr.chan@gmail.com>
2022-07-14 13:43:51 -0500

    added cart feats

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -6,14 +6,20 @@ class AddToCart extends React.Component {
     super(props);
     this.state = {
       size: null,
+      defaultQuant: null,
     }
   }
 
   sizeOptions(ops) {
     if (ops) {
-      return Object.values(ops).map((item, i) => {
+      var sizes = Object.values(ops).map((item, i) => {
         return <option key={i}>{item.size}</option>
       });
+      if (sizes.length > 0) {
+        return sizes;
+      } else {
+        return 'OUT OF STOCK';
+      }
     }
   }
   quantOptions(ops, size) {
@@ -32,25 +38,39 @@ class AddToCart extends React.Component {
 
   onChange(e) {
     this.setState({
-      size: e.target.value,
+      [e.target.id]: e.target.value,
     });
+    if (e.target.id === 'size' && e.target.value === 'Select Size') {
+      this.setState({
+        size: null,
+        defaultQuant: null,
+      });
+    }
+  }
+
+  addToCart() {
+    if (!this.state.size) {
+      alert('please select a size');
+    } else if (!this.state.defaultQuant) {
+      alert('please select a quantity')
+    }
   }
 
   render() {
     return(
       <div className="cart">
         <div id="selection">
-          <select onChange={this.onChange.bind(this)}>
+          <select id="size" onChange={this.onChange.bind(this)}>
             <option>Select Size</option>
             {this.sizeOptions(this.props.style)}
           </select>
-          <select id="defaultQuant" disabled="disabled">
+          <select id="defaultQuant" disabled="disabled" onChange={this.onChange.bind(this)}>
             <option>-</option>
             {this.quantOptions(this.props.style, this.state.size)}
           </select>
         </div>
         <div id="addToCart">
-          <button>Add to Cart</button>
+          <button onClick={this.addToCart.bind(this)}>Add to Cart</button>
           <button>star</button>
         </div>
       </div>
