4f138f7387076290983c65178b7a6eb0b32f1d15
Othniel Chan <othnielhr.chan@gmail.com>
2022-07-14 16:54:02 -0500

    added thumbnail click feature

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -1,29 +1,59 @@
 import React from 'react';
 
-var getImg = (link) => {
-  if (link) {
-    return link[0].url;
+class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currImg: null,
+      list: [],
+    }
+    this.thumbClick.bind(this);
   }
-}
-var getThumb = (link) => {
-  if (link) {
-    return link.map((img, i) => <img key={i} className="thumbs" src={img.thumbnail_url} />)
+
+  componentDidUpdate(prevProps) {
+    if(this.props.id !== prevProps.id) {
+      var arr = this.getThumb(this.props.style);
+      this.setState({
+        currImg: this.props.style[0].thumbnail_url,
+        list: arr,
+      });
+    }
   }
-}
-var ImageGallery = (props) => (
-  <div className="gallery">
-    <div>
-      <button>^</button>
-      {getThumb(props.style)}
-      <button>v</button>
-    </div>
-    <div className="img">
-      <button style={{height: '25px'}}>&lt;</button>
-      <img className="mainImg" src={getImg(props.style)} />
-      <button style={{height: '25px'}}>></button>
-    </div>
-  </div>
-);
+
+  getImg(link) {
+    if (link) {
+      return link[0].url;
+    }
+  }
+  getThumb(link) {
+    if (link) {
+      return link.map((img, i) => <img key={i} className="thumbs" src={img.thumbnail_url} onClick={this.thumbClick.bind(this)} />)
+    }
+  }
+
+  thumbClick(e) {
+    var clickedThumbLink = e.target.src;
+    this.setState({
+      currImg: clickedThumbLink,
+    });
+  }
+
+  render() {
+    return (
+      <div className="gallery">
+        <div>
+          <button>^</button>
+          {this.state.list}
+          <button>v</button>
+        </div>
+        <div className="img">
+          <button style={{height: '25px'}}>&lt;</button>
+          <img className="mainImg" src={this.state.currImg} />
+          <button style={{height: '25px'}}>></button>
+        </div>
+      </div>)
+  }
+};
 
 export default ImageGallery;
 
