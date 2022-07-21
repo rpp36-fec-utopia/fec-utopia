fdbe2ea9fa603abcfacd28445fb8591494f5b186
Othniel Chan <othnielhr.chan@gmail.com>
2022-07-21 15:41:38 -0500

    testing stars

---
+++ client/src/components/helpers/StarReview.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaStar, FaRegStar } from 'react-icons/fa';
+
+var StarReview = (props) => {
+  {console.log('hi')}
+  <div className="outer-star">
+    <FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar />
+    <div className="inner-star" style={{width: props.rating}}>
+    <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
+    </div>
+  </div>
+}
+
+export default StarReview;
