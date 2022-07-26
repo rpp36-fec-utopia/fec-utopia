b509d7ef149237464390e95300bf1c17686b12e3
Othniel Chan <othnielhr.chan@gmail.com>
2022-07-26 18:06:37 -0500

    zoom on clicked position

--- client/src/components/overview/ExpandedView.jsx
+++ client/src/components/overview/ExpandedView.jsx
@@ -6,22 +6,36 @@ class ExpandedView extends React.Component {
     this.state = {
       img: null,
       list: [],
+      zoom: false,
     };
     this.itsZoominTime.bind(this);
   }
 
   itsZoominTime(e) {
     // zoom here
-    var x = e.clientX - e.target.offsetLeft - 75;
-    var y = e.clientY - e.target.offsetTop - 25;
-    console.log(x, y)
+    var x = e.clientX + e.screenX + 75;
+    var y = e.clientY + e.screenY + 25;
+    var scaledX = x / 2.5;
+    var scaledY = y / 2.5;
+    var yperc = (scaledY * 100) / e.target.height;
+    var xperc = (scaledX * 100) / e.target.width;
+    console.log(x, y, scaledX, scaledY, e)
+    if (!this.state.zoom) {
+      document.getElementById('zoom').style.transform = `scale(2.5)`;
+      document.getElementById('zoom').style.transformOrigin = `${xperc}% ${yperc}%`;
+    } else {
+      document.getElementById('zoom').style.transform = 'scale(1)';
+    }
+    this.setState({
+      zoom: !this.state.zoom,
+    });
   }
 
   render() { // props: vis, close, fullList, currImg
     if (this.props.vis) {
       return (
-        <div className="modal">
-          <img className="imgModal" src={this.props.currImg} onClick={this.itsZoominTime.bind(this)}></img>
+        <div className="modal" id="asdf">
+          <img className="imgModal" id="zoom" src={this.props.currImg} onClick={this.itsZoominTime.bind(this)}></img>
           <button className="button" onClick={this.props.close}>x</button>
         </div>
       )
